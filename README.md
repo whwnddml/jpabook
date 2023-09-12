f8414467e6ccbfbfb2c896aadefd385cff424c01
whwnddml <junny4u@gmail.com>
2023-09-12 12:05:59 +0900

    Member 엔티티 추가 및 Junit 테스트

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# 프로젝트 기본 설정을 하고 테스트 합니다.
+# jpa-02 : h2 database 설정
+### 사이트에서 다운로드 후 로컬에 압축해제
+https://www.h2database.com/html/main.html  
+### 유저 홈 경로의 Documents 에 압축을 해제 함.
+~/Documents/h2
+### h2 database 실행(Mac)
+```bash
+$ sudo chmod +x h2.sh 
+$ ./h2.sh
+```
+### 관리화면(웹) 접속
+http://localhost:8082/  
+최초 : 세션유지하며 파일생성 jdbc url => jdbc:h2:~/jpashop  
+두번째 이후 : jdbc url => jdbc:h2:tcp://localhost/~/jpashop
+
+
+# jpa-01 : 프로젝트 기본 설정을 하고 테스트 합니다.
 - 확인 url  
 . http://localhost:8080  
 . http://localhost:8080/hello  
---
+++ src/test/java/com/book/jpa/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.book.jpa;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveID = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveID);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
