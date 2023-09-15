ccaa0678fc825ae571847b5197ef53447633f668
whwnddml <junny4u@gmail.com>
2023-09-15 10:37:11 +0900

    연관관계 메서드 추가

--- src/main/java/com/book/jpa/domain/Order.java
+++ src/main/java/com/book/jpa/domain/Order.java
@@ -35,6 +35,20 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
 
-    //===
+    //==연관관계 메서드==//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void setOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
